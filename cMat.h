7b191901e220838a0417d1a702551d8ec1be4078
Patrick Oare <patrickoare@berkeley.edu>
2017-02-14 01:24:45 -0800

    added division, toString, indexing, fixed up some unit tests

--- cMat.h
+++ cMat.h
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <cmath>
 #include <iostream>
+#include <stdexcept>
 
 #include <opencv2/core.hpp>
 #include <opencv2/imgproc.hpp>
@@ -15,163 +16,257 @@
 
 namespace cvc {
 
-  class cMat
-  {
-      private:
-        // Matrix properties
-        int8_t mType;
-        cv::Size mSize;
+    class cMat
+    {
+        private:
+            // Matrix properties
+            int8_t mType;
+            cv::Size mSize;
+        public:
 
-      public:
+            // Mat objects
+            cv::UMat real;
+            cv::UMat imag;
 
-          // Mat objects
-          cv::UMat real;
-          cv::UMat imag;
+            // Return size of real part as size
+            cv::Size size() {return mSize;};
 
-          // Return size of real part as size
-          cv::Size size() {return mSize;};
+            //Return type in the same way as UMat class
+            int8_t type() {return mType;};
 
-          //Return type in the same way as UMat class
-          int8_t type() {return mType;};
+            // Initialize with complex values
+            cMat(cv::UMat real_i, cv::UMat imag_i)
+            {
+                real = real_i.clone();
+                imag = imag_i.clone();
+                mType = real.type();
+                mSize = real.size();
+            }
 
-          // Initialize with complex values
-          cMat(cv::UMat real_i, cv::UMat imag_i)
-          {
-              real = real_i.clone();
-              imag = imag_i.clone();
-              mType = real.type();
-              mSize = real.size();
-          }
-
-          cMat(cv::Mat real_i, cv::Mat imag_i)
-              {cMat(real_i.getUMat(cv::ACCESS_RW), imag_i.getUMat(cv::ACCESS_RW));};
-
-
-          // Initialize with real values only
-          cMat(cv::UMat real_i)
-          {
-              real = real_i.clone();
-              mType = real.type();
-              mSize = real.size();
-              imag = cv::UMat::zeros(real_i.size(),real_i.type());
-          }
-          cMat(cv::Mat real_i)
-              {cMat(real_i.getUMat(cv::ACCESS_RW));};
-
-          // Initialize with zeros
-          cMat(cv::Size newSize, int8_t newType)
-          {
-              mType = newType;
-              mSize = newSize;
-              real = cv::UMat::zeros(mSize, mType);
-              imag = cv::UMat::zeros(mSize, mType);
-          }
-
-          // Helper function for fast assignment
-          friend void swap(cMat& first, cMat& second) // nothrow
-          {
-              // enable ADL (not necessary in our case, but good practice)
-              using std::swap;
-
-              // by swapping the members of two objects,
-              // the two objects are effectively swapped
-              swap(first.real, second.real);
-              swap(first.imag, second.imag);
-          }
-
-          cMat& operator=(cMat other) // (1)
-          {
-              swap(*this, other); // (2)
-              return *this;
-          }
-
-          // Arithmatic Operators
-
-          // Matrix element-wise subtraction
-          cMat& operator+=(const cMat& val)
-          {
-              cv::add(val.real, this->real, this->real);
-              cv::add(val.real, this->imag, this->imag);
-              return *this;
-          }
-
-          // Scaler element-wise subtraction
-          cMat& operator+=(const double& val)
-          {
-              cv::add(val, this->real, this->real);
-              cv::add(val, this->imag, this->imag);
-              return *this;
-          }
-
-          // Matrix element-wise subtraction
-          cMat& operator-=(const cMat& val)
-          {
-              cv::subtract(val.real, this->real, this->real);
-              cv::subtract(val.real, this->imag, this->imag);
-              return *this;
-          }
-
-          // Scaler element-wise subtraction
-          cMat& operator-=(const double& val)
-          {
-              cv::subtract(val, this->real, this->real);
-              cv::subtract(val, this->imag, this->imag);
-              return *this;
-          }
-
-          // Matrix element-wise multiplication
-          cMat& operator*=(const cMat& val)
-          {
-              cv::UMat tmp1;
-              cv::UMat tmp2;
-
-              cv::multiply(this->real, val.real, tmp1);
-              cv::multiply(this->imag, val.imag, tmp2);
-
-              cv::subtract(tmp1, tmp2, this->real);
-              cv::add(tmp1, tmp2, this->imag);
-
-              return *this;
-          }
+            cMat(cv::Mat real_i, cv::Mat imag_i)
+                {cMat(real_i.getUMat(cv::ACCESS_RW), imag_i.getUMat(cv::ACCESS_RW));};
 
 
-          // Scaler element-wise multiplication
-          cMat& operator*=(const double& val)
-          {
-              cv::multiply(val, this->real, this->real);
-              cv::multiply(val, this->imag, this->imag);
-              return *this;
-          }
+            // Initialize with real values only
+            cMat(cv::UMat real_i)
+            {
+                real = real_i.clone();
+                mType = real.type();
+                mSize = real.size();
+                imag = cv::UMat::zeros(real_i.size(),real_i.type());
+            }
 
-          /*
-          // Matrix element-wise division
-          cMat& operator/=(const cMat& mat)
-          {
-              cMat result(size, type);
-              cv::divide(val, *this, *this);
-              return *this;
-          }
-          */
+            cMat(cv::Mat real_i)
+                {cMat(real_i.getUMat(cv::ACCESS_RW));};
 
-          // Scaler element-wise division
-          cMat& operator/=(const double& val)
-          {
-              cv::divide(val, this->real, this->real);
-              cv::divide(val, this->imag, this->imag);
-              return *this;
-          }
+            // Initialize with zeros
+            cMat(cv::Size newSize, int8_t newType)
+            {
+                mType = newType;
+                mSize = newSize;
+                real = cv::UMat::zeros(mSize, mType);
+                imag = cv::UMat::zeros(mSize, mType);
+            }
 
-  };
+            // Helper function for fast assignment
+            friend void swap(cMat& first, cMat& second) // nothrow
+            {
+                // enable ADL (not necessary in our case, but good practice)
+                using std::swap;
 
-  void printOclPlatformInfo();
+                // by swapping the members of two objects,
+                // the two objects are effectively swapped
+                swap(first.real, second.real);
+                swap(first.imag, second.imag);
+            }
 
-  // Display functions
-  void cmshow(cMat matToShow, std::string windowTitle);
-  void mouseCallback_cmshow(int event, int x, int y, int, void* param);
+            cMat& operator=(cMat other) // (1)
+            {
+                swap(*this, other); // (2)
+                return *this;
+            }
 
-  cMat abs(cMat& inMat);
-  cMat angle(cMat& inMat);
-  cMat conj(cMat& inMat);
+            // Arithmatic Operators
+
+            // Matrix element-wise subtraction
+            cMat& operator+=(const cMat& val)
+            {
+                cv::add(val.real, this->real, this->real);
+                cv::add(val.real, this->imag, this->imag);
+                return *this;
+            }
+
+            friend cMat operator+(cMat lhs, const cMat& rhs) {
+                lhs += rhs;
+                return lhs;
+            }
+
+            friend cMat operator+(cMat lhs, const double& rhs) {
+                lhs += rhs;
+                return lhs;
+            }
+
+            friend cMat operator-(cMat lhs, const cMat& rhs) {
+                lhs -= rhs;
+                return lhs;
+            }
+
+            friend cMat operator-(cMat lhs, const double& rhs) {
+                lhs -= rhs;
+                return lhs;
+            }
+
+            friend cMat operator*(cMat lhs, const cMat& rhs) {
+                lhs *= rhs;
+                return lhs;
+            }
+
+            friend cMat operator*(cMat lhs, const double& rhs) {
+                lhs *= rhs;
+                return lhs;
+            }
+
+            friend cMat operator/(cMat lhs, const cMat& rhs) {
+                lhs /= rhs;
+                return lhs;
+            }
+
+            friend cMat operator/(cMat lhs, const double& rhs) {
+                lhs /= rhs;
+                return lhs;
+            }
+
+            friend std::ostream& operator<<(std::ostream& output, const cMat& mat) {
+                output << '\n' << mat.toString();
+                return output;
+            }
+
+            // Scaler element-wise subtraction
+            cMat& operator+=(const double& val)
+            {
+                cv::add(val, this->real, this->real);
+                cv::add(val, this->imag, this->imag);
+                return *this;
+            }
+
+            // Matrix element-wise subtraction
+            cMat& operator-=(const cMat& val)
+            {
+                cv::subtract(val.real, this->real, this->real);
+                cv::subtract(val.real, this->imag, this->imag);
+                return *this;
+            }
+
+            // Scaler element-wise subtraction
+            cMat& operator-=(const double& val)
+            {
+                cv::subtract(val, this->real, this->real);
+                cv::subtract(val, this->imag, this->imag);
+                return *this;
+            }
+
+            // Matrix element-wise multiplication
+            cMat& operator*=(const cMat& val)
+            {
+                cv::UMat tmp1;
+                cv::UMat tmp2;
+                cv::UMat tmp3;
+                cv::UMat tmp4;
+                cv::multiply(this->real, val.real, tmp1);
+                cv::multiply(this->imag, val.imag, tmp2);
+                cv::multiply(this->real, val.imag, tmp3);
+                cv::multiply(this->imag, val.real, tmp4);
+
+                cv::subtract(tmp1, tmp2, this->real);
+                cv::add(tmp3, tmp4, this->imag);
+
+                return *this;
+            }
+
+
+            // Scaler element-wise multiplication
+            cMat& operator*=(const double& val)
+            {
+                cv::multiply(val, this->real, this->real);
+                cv::multiply(val, this->imag, this->imag);
+                return *this;
+            }
+
+            // Matrix element-wise division
+            cMat& operator/=(const cMat& mat) {
+                cv::UMat temp;
+                cv::multiply(-1.0, mat.imag, temp);
+                cMat conjugate(mat.real, temp);
+                std::cout << conjugate.toString() << std::endl;
+                *this *= conjugate;
+                cMat divisor = mat;
+                std::cout << "divisor before is: " << '\n' << divisor.toString() << std::endl;
+                divisor *= conjugate;
+                cv::divide(this->real, divisor.real, this->real);
+                cv::divide(this->imag, divisor.real, this->imag);
+                return *this;
+            }
+
+            // Scaler element-wise division
+            cMat& operator/=(const double& val)
+            {
+                cv::divide(val, this->real, this->real);
+                cv::divide(val, this->imag, this->imag);
+                return *this;
+            }
+
+            // set the element at (m, n) equal to val
+            void set(int m, int n, std::complex<double> val) {
+                if (m > this->mSize.height || n > this->mSize.width) {
+                    throw std::invalid_argument("invalid matrix index");
+                }
+                this->real.getMat(cv::ACCESS_RW).at<float>(m, n) = val.real();
+                this->imag.getMat(cv::ACCESS_RW).at<float>(m, n) = val.imag();
+            }
+
+            std::complex<double>* get(int m, int n) const {
+                if (m > this->mSize.height || n > this->mSize.width) {
+                    throw std::invalid_argument("invalid matrix index");
+                }
+                double real = this->real.getMat(cv::ACCESS_READ).at<float>(m, n);
+                double im = this->imag.getMat(cv::ACCESS_READ).at<float>(m, n);
+                return new std::complex<double>(real, im);
+            }
+
+            // toString method for the cMat class.
+            std::string toString() const {
+                std::string s = "";
+                for (int r = 0; r < this->mSize.height; r++) {
+                    s += "[";
+                    for (int c = 0; c < this->mSize.width; c++) {
+                        std::complex<double>* z = this->get(r, c);
+                        std::string entry = std::to_string(z->real()) + " + " + std::to_string(z->imag()) + "*j";
+                        if (c != 0) {
+                            s += ", ";
+                        }
+                        s += entry;
+                    }
+                    s += "]";
+                    s += '\n';
+                }
+                return s;
+            }
+
+            cv::Size getSize() {
+                return this->mSize;
+            }
+
+        };
+
+    void printOclPlatformInfo();
+
+    // Display functions
+    void cmshow(cMat matToShow, std::string windowTitle);
+    void mouseCallback_cmshow(int event, int x, int y, int, void* param);
+
+    cMat abs(cMat& inMat);
+    cMat angle(cMat& inMat);
+    cMat conj(cMat& inMat);
 }
 
 
--- unit-test/cMat_test.cpp
+++ unit-test/cMat_test.cpp
@@ -5,7 +5,82 @@
 
 using namespace cvc;
 
+cMat* getMat(int m, int n, double low, double high) {
+    cv::UMat real(cv::Size(m, n), 5);
+    cv::UMat im(cv::Size(m, n), 5);
+    cv::randu(real, cv::Scalar(low), cv::Scalar(high));
+    cv::randu(im, cv::Scalar(low), cv::Scalar(high));
+    return new cMat(real, im);
+}
+
+cMat* getMat(int m, int n) {
+    return getMat(m, n, 0.0, +1.0);
+}
+
+cMat* getMat(int n) {
+    return getMat(n, n);
+}
+
+//test toString. 4x2 matrix.
+void testToString() {
+    std::cout << "Testing toString." << std::endl;
+    double low = 0.0;
+    double high = 1.0;
+    cv::UMat toMatR(cv::Size(2, 4), 5);
+    cv::UMat toMatI(cv::Size(2, 4), 5);
+    cv::randu(toMatR, cv::Scalar(low), cv::Scalar(high));
+    cv::randu(toMatI, cv::Scalar(low), cv::Scalar(high));
+    cMat toMat(toMatR, toMatI);
+    toMat.real = toMatR;
+    toMat.imag = toMatI;
+    std::cout << "toString test: " << '\n' << toMat.toString() << std::endl;
+}
+
+//test get
+void testGet() {
+    std::cout << "Testing Get." << std::endl;
+    int size = 5;
+    cMat* A = getMat(size);
+    std::cout << "A is: " << '\n' << A->toString() << std::endl;
+    for (int r = 0; r < size; r++) {
+        for (int c = 0; c < size; c++) {
+            std::complex<double>* z = A->get(r, c);
+            std::cout << "A(" << r << ", " << c << ") is: " << z->real() <<
+                " + i*" << z->imag() << std::endl;
+        }
+    }
+}
+
+//test set
+void testSet() {
+    std::cout << "Testing Set." << std::endl;
+    int size = 5;
+    cMat* A = getMat(size);
+    std::cout << "A is: " << '\n' << A->toString() << std::endl;
+    for (int r = 0; r < size; r++) {
+        for (int c = 0; c < size; c++) {
+            std::complex<double> z (r, c);
+            A->set(r, c, z);
+        }
+    }
+    std::cout << "A is: " << '\n' << A->toString() << std::endl;
+}
+
+// add test
+
+// subtraction test
+
+// multiplication test
+
+// division test
+
 int main(int argc, char** argv ){
+
+    testToString();
+    testGet();
+    testSet();
+
+    /*
     //printOclPlatformInfo();
 
     // Generate random matrix
@@ -19,16 +94,44 @@ int main(int argc, char** argv ){
     //testMat2 = cv::UMat::zeros(cv::Size(5,5), 5);
 
     cMat mat_complex(testMat,testMat2);
-    mat_complex.real = testMat;
-    mat_complex.imag = testMat2;
     std::cout << "Complex initialization - size: " << mat_complex.size() << std::endl;
 
     // Print first indicies of array
-    std::cout << "real: " << mat_complex.real.getMat(cv::ACCESS_READ).at<float>(0,0) <<std::endl;
-    std::cout << "imag: " << mat_complex.imag.getMat(cv::ACCESS_READ).at<float>(0,0) <<std::endl;
+//    std::cout << "real: " << mat_complex.real.getMat(cv::ACCESS_READ).at<float>(0,0) <<std::endl;
+//    std::cout << "imag: " << mat_complex.imag.getMat(cv::ACCESS_READ).at<float>(0,0) <<std::endl;
+
+    //Generate other random matrix
+    cv::UMat otherMat(cv::Size(5, 5), 5);
+    cv::UMat otherMat2(cv::Size(5, 5), 5);
+    cv::randu(otherMat, cv::Scalar(low), cv::Scalar(high));
+    cv::randu(otherMat2, cv::Scalar(low), cv::Scalar(high));
+    cMat matComplex2(otherMat, otherMat2);
+    matComplex2.real = otherMat;
+    matComplex2.imag = otherMat2;
+
+//    std::cout << "First matrix at 0, 0 has real part " << mat_complex.real.getMat(cv::ACCESS_READ).at<float>(0, 0)
+//        << " and imaginary part " << mat_complex.imag.getMat(cv::ACCESS_READ).at<float>(0, 0) << std::endl;
+//    std::cout << "Second matrix at 0, 0 has real part " << matComplex2.real.getMat(cv::ACCESS_READ).at<float>(0, 0)
+//        << " and imaginary part " << matComplex2.imag.getMat(cv::ACCESS_READ).at<float>(0, 0) << std::endl;
+    std::cout << "First matrix is: " << '\n' << mat_complex.toString() << std::endl;
+    std::cout << "Second matrix is: " << '\n' << matComplex2.toString() << std::endl;
+
+    //uncomment for division tests
+    mat_complex /= matComplex2;
+    std::cout << "After division: " << '\n' << mat_complex.toString() << std::endl;
+
+    std::cout << mat_complex.toString() << std::endl;
 
 
-    cmshow(mat_complex,"Complex Mat");
+    cvc::conj(mat_complex);
+
+//    cmshow(mat_complex,"Complex Mat");
+    std::cout << "overloading test: " << std::endl;
+    std::cout << toMat << std::endl;
+
+    std::cout << "random mat test: " << std::endl;
+    std::cout << getMat(5) << std::endl;
+    */
 
 
 }
